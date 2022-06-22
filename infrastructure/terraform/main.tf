80903df3c7d68e8a1f0f9d15d8d4d9f1bfb89f22
Domenic Gosein <87760323+goseind@users.noreply.github.com>
2022-06-22 11:00:39 +0200

    add rancher2 provider

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,1 +1,12 @@
-# tbd
+terraform {
+  required_providers {
+    rancher2 = {
+      source = "rancher/rancher2"
+      version = "1.24.0"
+    }
+  }
+}
+
+provider "rancher2" {
+  # Configuration options
+}
